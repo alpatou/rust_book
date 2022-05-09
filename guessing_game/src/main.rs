cf5aa05632d9d26c17f71897ae68bc283219bae3
patal <patourasalexandros@gmail.com>
2022-05-09 23:51:30 +0300

    incomplete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -12,5 +17,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
-}
 
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
